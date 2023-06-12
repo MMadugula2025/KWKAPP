ed2e9087203581902695c4926a1460b07705e4b4
Sonya <sonyahasthantra@gmail.com>
2023-06-12 14:48:01 -0700

    Initial Commit

---
+++ Final_Project_ResponsiPILL/Final_Project_ResponsiPILLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_ResponsiPILLApp.swift
+//  Final_Project_ResponsiPILL
+//
+//  Created by Dilan Hasthantra on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_ResponsiPILLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
