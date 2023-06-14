34c394c1ce47c119d61b42c85364d4706ec28ba2
Sonya <sonyahasthantra@gmail.com>
2023-06-14 12:31:44 -0700

    Added Quiz

--- Final_Project_ResponsiPILL/Journal.swift
+++ Final_Project_ResponsiPILL/Journal.swift
@@ -8,40 +8,65 @@
 import SwiftUI
 
 struct Journal: View {
-    var body: some View {
-        NavigationStack {
-            VStack {
-                Text("Welcome to the Journal!")
-                    .fontWeight(.bold)
-                Text("This is the Journal page.")
-                    
-                
-                
-            }
-            .toolbar {
-                    ToolbarItemGroup(placement: .bottomBar) {
-                    NavigationLink(destination: Information()) {
-                                    Text("Information")
-//                            .padding([.top, .leading])
-                                }
-                        Spacer()
-                        NavigationLink(destination: Reminders()) {
-                                        Text("Reminders")
-//                                .padding([.top, .leading, .trailing])
-                                    }
-                        Spacer()
-                        NavigationLink(destination: ContentView()) {
-                                        Text("Home")
-//                                .padding([.top, .trailing])
-                                    }
-                        Spacer()
-                                    }
-            }
-                            }
-            
-        }
-    }
     
+    @State private var entry = ""
+    @State private var textTitle = "How are you feeling?"
+    
+    var body: some View {
+            NavigationStack {
+                VStack {
+                    
+                    Text("ResponsiPILL")
+                        .font(.largeTitle)
+                    Text("\(textTitle)")
+                        .font (.title)
+                        .padding(.top)
+                    
+                    //textfield
+                    TextField("", text: $entry)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .border(Color.purple, width: 1)
+                        .padding()
+                    
+                    //button
+                    Button("Submit") {
+                        print(entry)
+                        textTitle = "\(entry)!"
+                        
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.black)
+                    
+                    
+                    .toolbar {
+                        ToolbarItemGroup(placement: .bottomBar) {
+                            NavigationLink(destination: Information()) {
+                                Text("Information")
+                                //                            .padding([.top, .leading])
+                            }
+                            Spacer()
+                            NavigationLink(destination: Reminders()) {
+                                Text("Reminders")
+                                //                                .padding([.top, .leading, .trailing])
+                            }
+                            Spacer()
+                            NavigationLink(destination: ContentView()) {
+                                Text("Home")
+                                //                                .padding([.top, .trailing])
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+                
+            }
+        
+        
+            }
+        }
+
 
 struct Journal_Previews: PreviewProvider {
     static var previews: some View {
